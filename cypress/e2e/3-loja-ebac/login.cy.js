dac26c1938bc85867cb44585456a0078f2758f04
Bruno Noberto <brunonoberto@Brunos-MBP.Home>
2024-02-06 16:32:52 +0000

    adicionado arquivo de dados e padronizacao da baseUrl

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress"/>
 
+const perfil = require('../../fixtures/perfil.json')
+
 // Describe é a função que representa a funcionalidade e o it são os cenarios de teste
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta/')
     });
 
     afterEach(() => {
@@ -33,5 +35,12 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail bruno.teste@ebac.com.br está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error').should('exist')
     });
+
+    it('Deve fazer login com sucesso - Utilizando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)').should('contain', 'bruno.teste-9138')
+    });   
 
 });
