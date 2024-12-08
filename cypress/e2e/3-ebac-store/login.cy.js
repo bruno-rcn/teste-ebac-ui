9c397b6c9636983dc228d0630375e60267ddb4ed
Bruno Noberto <brunorcnoberto@hotmail.com>
2024-12-08 13:36:48 +0000

    add login error and hooks

--- cypress/e2e/3-ebac-store/login.cy.js
+++ cypress/e2e/3-ebac-store/login.cy.js
@@ -2,11 +2,34 @@
 
 // This is where the test are being created. Each on 'it' is a test case
 describe('Feature: Login', () => {
-    it('Should do success login', () => {
+
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    })
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Should do success login', () => {
         cy.get('#username').type('qa@teste.pt')
         cy.get('#password').type('123456')
         cy.get('.woocommerce-form > .button').click() // You need the parentheses because it is a method. You just don't need any parameters
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, qa-4406 (não é qa-4406? Sair)')
+    });
+
+    it('Login with wrong email', () => {
+        cy.get('#username').type('br@teste.pt')
+        cy.get('#password').type('123456')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+    });
+
+    // Use it.only when you wanna that execute just this specific test
+    it.only('Login with wrong password', () => {
+        cy.get('#username').type('qa@teste.pt')
+        cy.get('#password').type('111111')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail qa@teste.pt está incorreta. Perdeu a senha?')
     });
 });
