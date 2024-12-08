e34132482cf0c2fcf6d9810f78df9cfbb86ddc3e
Bruno Noberto <brunorcnoberto@hotmail.com>
2024-12-08 14:29:07 +0000

    add data faker

---
+++ cypress/e2e/3-ebac-store/signin-up.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress"/> // Reference to use of cypress in this file
+
+import { faker } from '@faker-js/faker';
+
+describe('Feature: Signin up', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    })
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Signin', () => {
+
+        var lastName = faker.person.lastName()
+
+        cy.get('#reg_email').type(faker.internet.email()) // generate fake email
+        cy.get('#reg_password').type('123456')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(lastName) // using variables
+
+        cy.wait(5000) // wait for 5 seconds before continue the script
+    });
+});
